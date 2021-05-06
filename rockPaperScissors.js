f75ab6dffc41bac714967e96e7e0bb446ea48b5c
MechanicalEngineer2Coder <powerguitar2009@gmail.com>
2021-05-06 10:38:15 -0400

    Simple rock, paper, scissors game.

---
+++ rockPaperScissors.js
@@ -0,0 +1,50 @@
+function game(){
+    var userSelection = document.getElementById("userSelection").value;
+    console.log(document.getElementById("userSelection").value);
+
+    var computerSelection = Math.floor(Math.random() * 3) + 1;
+    console.log(computerSelection);
+    if (computerSelection==1){
+        computerSelection = 'rock';
+    } else if (computerSelection==2){
+        computerSelection = "paper";
+    } else{
+        computerSelection = "scissors";
+    }
+
+    document.getElementById("computerSelection").innerHTML = computerSelection[0].toUpperCase() + computerSelection.slice(1);
+
+    console.log(userSelection + " " + computerSelection)
+
+    if (userSelection=='rock' && computerSelection == 'rock') {
+        document.getElementById("results").innerHTML = "Tie";
+        console.log("Tie");
+    } else if (userSelection=='rock' && computerSelection == 'paper') {
+        document.getElementById("results").innerHTML = "Computer Wins";
+        console.log("Computer wins");
+    } else if (userSelection=='rock' && computerSelection == 'scissors') {
+        document.getElementById("results").innerHTML = "Player Wins";
+        console.log("Player wins");
+    } else if (userSelection=='paper' && computerSelection == 'rock') {
+        document.getElementById("results").innerHTML = "Player Wins";
+        console.log("Player wins");
+    } else if (userSelection=='paper' && computerSelection == 'paper') {
+        document.getElementById("results").innerHTML = "Tie";
+        console.log("Tie");
+    } else if (userSelection=='paper' && computerSelection == 'scissors') {
+        document.getElementById("results").innerHTML = "Computer Wins";
+        console.log("Computer win");
+    } else if (userSelection=='scissors' && computerSelection == 'rock') {
+        document.getElementById("results").innerHTML = "Computer Wins";
+        console.log("Computer wins");
+    } else if (userSelection=='scissors' && computerSelection == 'paper') {
+        document.getElementById("results").innerHTML = "Player Wins";
+        console.log("Player wins");
+    } else if (userSelection=='scissors' && computerSelection == 'scissors') {
+        document.getElementById("results").innerHTML = "Tie";
+        console.log("Tie");
+    } else{
+        document.getElementById("results").innerHTML = "Something went wrong.";
+        console.log("Something went wrong.");
+    }
+}
