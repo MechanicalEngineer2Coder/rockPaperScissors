e321bd671dfbc615ae383c67a8c6084255de0435
MechanicalEngineer2Coder <powerguitar2009@gmail.com>
2021-05-06 13:50:44 -0400

    Changed innerHTML's to innerText's.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -12,39 +12,39 @@ function game(){
         computerSelection = "scissors";
     }
 
-    document.getElementById("computerSelection").innerHTML = computerSelection[0].toUpperCase() + computerSelection.slice(1);
+    document.getElementById("computerSelection").innerText = computerSelection[0].toUpperCase() + computerSelection.slice(1);
 
     console.log(userSelection + " " + computerSelection)
 
     if (userSelection=='rock' && computerSelection == 'rock') {
-        document.getElementById("results").innerHTML = "Tie";
+        document.getElementById("results").innerText = "Tie";
         console.log("Tie");
     } else if (userSelection=='rock' && computerSelection == 'paper') {
-        document.getElementById("results").innerHTML = "Computer Wins";
+        document.getElementById("results").innerText = "Computer Wins";
         console.log("Computer wins");
     } else if (userSelection=='rock' && computerSelection == 'scissors') {
-        document.getElementById("results").innerHTML = "Player Wins";
+        document.getElementById("results").innerText = "Player Wins";
         console.log("Player wins");
     } else if (userSelection=='paper' && computerSelection == 'rock') {
-        document.getElementById("results").innerHTML = "Player Wins";
+        document.getElementById("results").innerText = "Player Wins";
         console.log("Player wins");
     } else if (userSelection=='paper' && computerSelection == 'paper') {
-        document.getElementById("results").innerHTML = "Tie";
+        document.getElementById("results").innerText = "Tie";
         console.log("Tie");
     } else if (userSelection=='paper' && computerSelection == 'scissors') {
-        document.getElementById("results").innerHTML = "Computer Wins";
+        document.getElementById("results").innerText = "Computer Wins";
         console.log("Computer win");
     } else if (userSelection=='scissors' && computerSelection == 'rock') {
-        document.getElementById("results").innerHTML = "Computer Wins";
+        document.getElementById("results").innerText = "Computer Wins";
         console.log("Computer wins");
     } else if (userSelection=='scissors' && computerSelection == 'paper') {
-        document.getElementById("results").innerHTML = "Player Wins";
+        document.getElementById("results").innerText = "Player Wins";
         console.log("Player wins");
     } else if (userSelection=='scissors' && computerSelection == 'scissors') {
-        document.getElementById("results").innerHTML = "Tie";
+        document.getElementById("results").innerText = "Tie";
         console.log("Tie");
     } else{
-        document.getElementById("results").innerHTML = "Something went wrong.";
+        document.getElementById("results").innerText = "Something went wrong.";
         console.log("Something went wrong.");
     }
 }
